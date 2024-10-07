cf57bb59331546b32f7f8d6c34a88c5cc9177520
AndresChCy <aichaparrom@gmail.com>
2024-10-07 07:37:04 -0300

    TTD: Unit test inicial para crear funcion pintar luego en Dibujo

---
+++ Dibujo.py
@@ -0,0 +1,13 @@
+class Dibujo:
+    def __init__(self , x ,y):
+        pass
+    def __init__(self ,matriz):
+        pass
+    def pintar(self, x, y,color):
+        pass
+
+    def comprimir(self, x, y, color):
+        pass
+    def getMatriz(self):
+        pass
+
