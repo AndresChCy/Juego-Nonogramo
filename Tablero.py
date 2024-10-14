13496cd099129becbabe454b4865dc5a11755a14
unknown <matgarcia2023@inf.udec.cl>
2024-10-13 22:20:55 -0300

    Agregacion de funcion guardarProgreso la cual se encarga de escribir la nueva informacion en la matriz

--- Tablero.py
+++ Tablero.py
@@ -31,5 +31,11 @@ class Tablero:
                 matriz.append(linea.strip().split())
             return matriz
 
-    def guardarProgreso(self):
-        pass
+    def guardarProgreso(self, matriz, directorio):
+            with open(directorio, "w") as f:
+                for i in range(len(matriz)):
+                    for j in range(len(matriz[i])):
+                        f.write(str(matriz[i][j]))
+                        if j < len(matriz[i]) - 1:
+                            f.write(" ")
+                    f.write("\n")
