994d022b564faad002a403432779fb3f6ebe5c09
unknown <matgarcia2023@inf.udec.cl>
2024-10-13 22:16:41 -0300

    Agregación de función cargarProgreso la cual se ebcarga de leer los datos

--- Tablero.py
+++ Tablero.py
@@ -17,3 +17,19 @@ class Tablero:
 
     def Compresion(self):
         pass
+
+    def pintarProgreso(self):
+        pass
+
+    def cargarProgreso(directorio):
+            with open(directorio, 'r') as f:
+                datos = f.readlines()
+
+            matriz = []
+            for linea in datos:
+                # Eliminamos el salto de línea y dividimos por espacios (o puedes usar otro separador)
+                matriz.append(linea.strip().split())
+            return matriz
+
+    def guardarProgreso(self):
+        pass
