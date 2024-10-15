e1dbf20663b9d551049b683c29bc9202dd9f0a83
unknown <matgarcia2023@inf.udec.cl>
2024-10-14 22:40:02 -0300

    TDD Se implementa test que falla para Compresión

--- test/tablero_test.py
+++ test/tablero_test.py
@@ -11,6 +11,13 @@ class tablero_test(unittest.TestCase):
         tablero = Tablero(solucion)
         self.assertEqual(tablero.CompararDibujos(), True)  # add assertion here
 
+    def test_compresion(self):
+        solucion = Dibujo(1, 2)
+        solucion.pintar(0, 0, 1)
+        tablero = Tablero(solucion)
+        comprVert, comprHor = tablero.Compresion()
+        self.assertEqual(comprHor, [[1],[0]])
+        self.assertEqual(comprVert, [1])
 
 if __name__ == '__main__':
     unittest.main()
