c5015e4f650db2c9ea587ffe35197abd29becb51
CondorPepinillo <marttapia2023@udec.cl>
2024-10-15 22:22:41 -0300

    TDD: se crea test que falla para clase niveles

---
+++ Niveles.py
@@ -0,0 +1,3 @@
+class Niveles:
+    def __init__(self):
+        pass
---
+++ test/niveles_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from Niveles import Niveles
+from matrizTotext import MatrizToText
+
+
+def test_niveles( tmp_path):
+    matriz_to_text = MatrizToText()
+    matriz = [['1', '0', '1'], ['0', '1', '0'], ['1', '1', '1']]
+    archivo = tmp_path / "matrizToTextTest.txt"
+
+    # Llamar al método
+    matriz_to_text.matrizToText(matriz, archivo)
+    niveles = Niveles(tmp_path)
+    assert (len(niveles.getList()),1)
+    assert((niveles.getList()[0].getSol()),matriz)
+
+
