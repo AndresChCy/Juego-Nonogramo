69d123e2ada363c246fb3e707e62882f3606de46
Bandido209 <estebananm2005@gmail.com>
2024-10-30 20:27:17 -0300

    Añadiendo archivo con la finalidad de separar y optimizar el codigo para MenuCrearNiveles.

---
+++ srcs/Visuals/MenuCrearNivel.py
@@ -0,0 +1,128 @@
+import pygame
+import sys
+from Panel import Panel
+from pygame.locals import *
+
+pygame.init()
+ventana = pygame.display.set_mode((800, 600), 0, 32)
+font = pygame.font.SysFont(None, 40)
+font_2 = pygame.font.SysFont(None, 32)
+
+pygame.display.set_caption("Nonogram")
+fpsControlador = pygame.time.Clock()
+
+class CrearNivel(Panel):
+    def __init__(self, ventana, titulo, ancho, largo):
+        self.ventana = ventana
+        self.titulo = titulo
+        self.ancho = ancho
+        self.largo = largo
+
+        self.active1 = False
+        self.active2 = False
+
+        self.text1 = ''
+        self.text2 = ''
+
+        self.color_inactive = pygame.Color('lightskyblue3')
+        self.color_active = pygame.Color('dodgerblue2')
+        self.color1 = self.color_inactive
+        self.color2 = self.color_inactive
+
+        self.rect = pygame.Rect((ventana.get_width() - 400) // 2, 200, 400, 250)
+        self.font = pygame.font.Font(None, 32)
+        self.clock = pygame.time.Clock()
+
+        self.input_box1 = pygame.Rect(self.rect.x + (self.rect.w - 140) // 2, self.rect.y + (self.rect.h - 32) // 3, 140, 32)
+        self.input_box2 = pygame.Rect(self.rect.x + (self.rect.w - 140) // 2, self.rect.y + (self.rect.h - 32) // 3 + 50, 140, 32)
+        self.boton_aceptar = pygame.Rect((ventana.get_width() - 200) // 2, ventana.get_height() - 70, 200, 50)
+
+    def execute(self):
+        running = True
+        while running:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    running = False
+                elif evento.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_click(evento.pos, evento.button)
+                elif evento.type == pygame.MOUSEMOTION:
+                    self.handle_mouse_motion(evento)
+                elif evento.type == pygame.KEYDOWN:
+                    self.handle_key(evento)
+
+            self.ventana.fill((0, 0, 0))
+            self.draw()
+            pygame.display.flip()
+        pygame.quit()
+        sys.exit()
+
+    def handle_mouse_motion(self,event):
+        pass
+
+    def handle_click(self, pos, button):
+        if self.input_box1.collidepoint(pos):
+            self.active1 = not self.active1
+        else:
+            self.active1 = False
+
+        if self.input_box2.collidepoint(pos):
+            self.active2 = not self.active2
+        else:
+            self.active2 = False
+
+        if self.boton_aceptar.collidepoint(pos):
+            print("Botón Aceptar presionado")
+
+        self.color1 = self.color_active if self.active1 else self.color_inactive
+        self.color2 = self.color_active if self.active2 else self.color_inactive
+
+    def handle_key(self,event):
+        if event.key == pygame.K_RETURN:
+            if self.text1 == '' or self.text2 == '':
+                return False, False
+            else:
+                return int(self.text1), int(self.text2)
+
+        if self.active1:
+            if event.key == pygame.K_BACKSPACE:
+                self.text1 = self.text1[:-1]
+            elif event.unicode.isdigit():
+                self.text1 += event.unicode
+
+        elif self.active2:
+            if event.key == pygame.K_BACKSPACE:
+                self.text2 = self.text2[:-1]
+            elif event.unicode.isdigit():
+                self.text2 += event.unicode
+
+    def draw(self):
+        pygame.draw.rect(self.ventana, (0, 0, 0), self.rect)
+        self.draw_text('Escoger dimensiones', font, (255, 255, 255), self.ventana, self.rect.centerx, self.rect.top + 20)
+
+        txt_surface1 = font.render(self.text1, True, self.color1)
+        txt_surface2 = font.render(self.text2, True, self.color2)
+
+        self.draw_text("Ancho:", font_2, (255, 255, 255), self.ventana, self.input_box1.left - 50, self.input_box1.centery)
+        self.draw_text("Alto:", font_2, (255, 255, 255), self.ventana, self.input_box2.left - 50, self.input_box2.centery)
+
+        self.ventana.blit(txt_surface1, (self.input_box1.x + 5, self.input_box1.y + 5))
+        self.ventana.blit(txt_surface2, (self.input_box2.x + 5, self.input_box2.y + 5))
+
+        pygame.draw.rect(self.ventana, (255,0,0), self.boton_aceptar)
+        self.draw_text("Aceptar", font, (255, 255, 255), self.ventana, self.boton_aceptar.centerx,self.boton_aceptar.centery)
+
+        pygame.draw.rect(self.ventana, self.color1, self.input_box1, 2)
+        pygame.draw.rect(self.ventana, self.color2, self.input_box2, 2)
+
+        pygame.display.flip()
+        self.clock.tick(30)
+
+    def draw_text(self,texto, font, color, superficie, x, y):
+        textobj = font.render(texto, 1, color)
+        textrect = textobj.get_rect(center=(x, y))
+        superficie.blit(textobj, textrect)
+
+if __name__ == "__main__":
+    ventana = pygame.display.set_mode((800, 600), 0, 32)
+    crear_nivel = CrearNivel(ventana,"Dimensiones", 8, 8)
+    crear_nivel.execute()
